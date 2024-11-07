b7272f8d09b1444db1472bfcf3417a0ecb5fc7cc
Max Pilipovic <84630222+MaxPilipovic@users.noreply.github.com>
2024-11-06 17:11:20 -0800

    Changed coloring and themes and made it a darker theme. Also added embedded video to showcase podcast.

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle("light");
+  }
+  
+  const darkModeButton = document.querySelector('#dark-mode-toggle');
+  if (darkModeButton) {
+    darkModeButton.addEventListener('click', toggleDarkMode);
+  }
