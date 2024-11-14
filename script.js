a785f1b9d85f98fda0f974ddf5a4bf62a9c64a53
Max Pilipovic <84630222+MaxPilipovic@users.noreply.github.com>
2024-11-13 17:02:09 -0800

    Made everything purple with new icons

--- script.js
+++ script.js
@@ -4,14 +4,4 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
-
-  function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle("light");
-  }
-  
-  const darkModeButton = document.querySelector('#dark-mode-toggle');
-  if (darkModeButton) {
-    darkModeButton.addEventListener('click', toggleDarkMode);
-  }
   
